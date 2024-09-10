c31bbe7815d77a76696b7565cdf98f1a59cc7088
KISHI Shinsuke <kishi@example.com>
2024-09-11 07:06:05 +0900

    Initial files.

---
+++ src/main/kotlin/com/example/quo_digital_demo/QuoDigitalDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.quo_digital_demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class QuoDigitalDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<QuoDigitalDemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/quo_digital_demo/QuoDigitalDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.quo_digital_demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class QuoDigitalDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
